3545b6042048238396846fbf916a413442febe41
posice <ilayyasi@gmail.com>
2023-03-02 20:32:18 +0100

    h

--- src/control/MainController.java
+++ src/control/MainController.java
@@ -42,6 +42,14 @@ public class MainController {
     public String searchList(String name){
         String output = "Nicht gefunden.";
         //TODO 01: Schreibe einen Suchalgorithmus
+        allPersons.toFirst();
+        while (allPersons.hasAccess()){
+            if (allPersons.getContent().getName().equals(name)){
+                output = allPersons.getContent().getName();
+                return output;
+            }
+            allPersons.next();
+        }
         return output;
     }
 
@@ -51,8 +59,52 @@ public class MainController {
      */
     public void sortList(){
         //TODO 02: Schreibe einen Sortieralgorithmus
+        List<Person> helpList = new List<>();
+        while (!allPersons.isEmpty()){
+            allPersons.toFirst();
+            Person first  = allPersons.getContent();
+            while (allPersons.hasAccess()){
+                if (allPersons.getContent().getName().compareTo(first.getName()) < 0 ){
+                    first = allPersons.getContent();
+                }
+                allPersons.next();
+            }
+            helpList.append(first);
+            allPersons.toFirst();
+            while (allPersons.getContent().getName().compareTo(first.getName())!= 0){
+                allPersons.next();
+            }
+            allPersons.remove();
+        }
+        allPersons.concat(helpList);
+        // Stabil und in-Place, da ich bei gleichem Namen immer den erst genannten Namen nach vorne packe und immer nur eine Liste erstelle
     }
 
+    private List<Person> quicksortList(List<Person> list){
+        list.toFirst();
+        list.next();
+        if (list.isEmpty() || !list.hasAccess()){
+            return list;
+        }
+        list.toFirst();
+        Person pivot = list.getContent();
+        list.remove();
+        List<Person> left = new List<>();
+        List<Person> right = new List<>();
 
+        while (list.hasAccess()){
+            if (list.getContent().getName().compareTo(pivot.getName())<= 0) left.append(list.getContent());
+            else                                                            right.append(list.getContent());
+            list.remove();
+        }
 
+        left = quicksortList(left);
+        right = quicksortList(right);
+
+        list.concat(left);
+        list.append(pivot);
+        list.concat(right);
+
+        return list;
+    }
 }
